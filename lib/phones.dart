0425508463efb7514da05554a864e16bec2b06e3
dfxjdhjxj <bequitebtw@mail.ru>
2024-02-28 03:35:29 +0300

    grid and scroll adding, navigation panel and floating button

--- lib/phones.dart
+++ lib/phones.dart
@@ -1,8 +1,3 @@
-import 'dart:ui';
-import 'package:flutter/widgets.dart';
-import 'package:phones/position.dart';
-import 'package:flutter/material.dart';
-
 class phone {
   String name;
   int price;
@@ -13,15 +8,15 @@ class phone {
 }
 
 List<phone> phonesList = [
-  phone("Iphone", 100000, "13", 120, 'assets/images/iphone.jpg'),
   phone("Xiaomi", 110000, "14pro", 120, 'assets/images/xiaomi.jpg'),
   phone("Honor", 30000, "90", 90, 'assets/images/honor.jpg'),
   phone("OnePlus", 60000, "10 pro", 120, 'assets/images/onePlus.jpg'),
   phone("Meizu", 20000, "20", 60, 'assets/images/meizu.jpg'),
   phone("Samsung", 80000, "s23 ultra", 120, 'assets/images/samsung.jpg'),
-  phone("Nokia", 3000, "3310", 60, 'assets/images/nokia3310.jpg'),
+  phone("Nokia", 3000, "3310", 10, 'assets/images/nokia3310.jpg'),
   phone("Huawei", 40000, "p30 pro", 120, 'assets/images/huawei.jpg'),
   phone("Oppo", 30000, "Find X7", 120, 'assets/images/oppo.jpg'),
   phone("Realme", 35000, "11 pro plus", 60, 'assets/images/realme.jpg'),
-  phone("ST", 1000000, "qweqweqwe", 2222, 'assets/images/satandart.png'),
 ];
+
+phone asd = phone("Iphone", 100000, "13", 120, 'assets/images/iphone.jpg');
--- lib/position.dart
+++ lib/position.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:phones/main.dart';
 import 'package:phones/phones.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    List<PhoneWidget> items = [];
+    for (int x = 0; x < phonesList.length; x++) {
+      items.add(PhoneWidget(
+        name: phonesList[x].name,
+        price: phonesList[x].price,
+        model: phonesList[x].model,
+        screenHertz: phonesList[x].screenHertz,
+        image: phonesList[x].image,
+      ));
+    }
+    final gridcounter = 2;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            "Phones",
-            style: TextStyle(color: Colors.black),
+            "DNS",
+            style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
-          backgroundColor: Colors.grey,
+          backgroundColor: Colors.orange,
         ),
-        body: Container(
-          color: Colors.grey,
-          child: ListView.builder(
-            itemCount: phonesList.length,
-            itemBuilder: (BuildContext context, index) {
-              return PhoneWidget(
-                name: phonesList[index].name,
-                model: phonesList[index].model,
-                price: phonesList[index].price,
-                screenHertz: phonesList[index].screenHertz,
-                image: phonesList[index].image,
-              );
-            },
+        body: SingleChildScrollView(
+          child: Container(
+            height: 630,
+            child: GridView.count(
+              crossAxisCount: gridcounter,
+              children: items,
+            ),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.more_horiz,
+          ),
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.black,
+          focusElevation: 100,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.orange,
+          child: Container(
+            height: 20.0,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       ),
     );
   }
@@ -45,7 +68,7 @@ class PhoneWidget extends StatelessWidget {
       this.model = "unknown",
       this.price = 0,
       this.screenHertz = 60,
-      this.image = 'assets/images/satandart.png'});
+      this.image = 'assets/images/standart.png'});
   final String name;
   final int price;
   final String model;
@@ -55,19 +78,22 @@ class PhoneWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
         child: Container(
-      height: 300,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
             this.name,
-            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 10),
           ),
-          Container(
-            decoration: BoxDecoration(color: Colors.grey),
-            height: 150,
-            width: 150,
-            child: Image(image: AssetImage(this.image)),
+          Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255)),
+              child: Image(image: AssetImage(this.image)),
+            ),
           ),
           Text('Стоимость: ${this.price}'),
           Text('Модель: ${this.model}'),
